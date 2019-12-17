bc617a8964ea84d0a36576482d79ef0b487f4379
Tyra-hans <tyrahans17@gmail.com>
2019-12-17 13:37:59 +0300

    Created a method setClasses() and bound it using the ngClass directive to allow us to add interactiveness to the completion of tasks checkboxes

--- angular-crash-todolist/src/app/components/todoitem/todoitem.component.ts
+++ angular-crash-todolist/src/app/components/todoitem/todoitem.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit ,Input} from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { splitClasses } from '@angular/compiler';
 
 @Component({
   selector: 'app-todoitem',
@@ -13,4 +14,13 @@ export class TodoitemComponent implements OnInit {
   ngOnInit() {
   }
 
+  // set dynamic classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-completed': this.todo.completed
+    }
+    return classes;
+  }
+  
 }
