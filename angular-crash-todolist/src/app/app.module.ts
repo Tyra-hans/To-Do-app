f1ce2bdbefa94154307097ebb865b1819862762b
Tyra-hans <tyrahans17@gmail.com>
2019-12-17 15:49:47 +0300

    Adds a header and a form for users to add items into the todo list

--- angular-crash-todolist/src/app/app.module.ts
+++ angular-crash-todolist/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoitemComponent } from './components/todoitem/todoitem.component';
 import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoitemComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
