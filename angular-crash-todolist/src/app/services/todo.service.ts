193d5fdca9a18e09e2478c11977fb93ed937552d
Tyra-hans <tyrahans17@gmail.com>
2019-12-17 14:08:16 +0300

    Displays Data from the json place holder and creates a limit to the todo items in the list

--- angular-crash-todolist/src/app/services/todo.service.ts
+++ angular-crash-todolist/src/app/services/todo.service.ts
@@ -1,29 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos() {
-    return  [
-      {
-        id: 1,
-        title: 'ToDo one',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'ToDo two',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'ToDo three',
-        completed: true
-      },
-    ]
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
